b3106cc4bfe0601f9c3d796506050e15e83c07f5
KinHang To <to.kin.hang@msn.com>
2015-12-27 02:26:30 +0800

    Initial Commit

---
+++ himawari8Wallpaper/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  himawari8Wallpaper
+//
+//  Created by ToKin-Hang on 27/12/2015.
+//  Copyright © 2015 to kin hang. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ himawari8Wallpaper/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  himawari8Wallpaper
+//
+//  Created by ToKin-Hang on 27/12/2015.
+//  Copyright © 2015 to kin hang. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
